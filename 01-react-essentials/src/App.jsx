74e5716fce57f3d0a20ebad9d9940f207a3a9af9
Shashi5657 <sangepushashidhar@gmail.com>
2024-02-11 16:25:39 +0530

    react essentials topic finished

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -38,43 +38,62 @@ function App() {
       <main>
         <section id="core-concepts">
           <ul>
-
-     {/* //we r passing title description, image as props to the CoreConcept component,
+            {/* //here used mapping function to display the data from coreConcepts to here */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* //we r passing title description, image as props to the CoreConcept component,
              so we can display other'd data with the same styling & everything */}
-            <CoreConcept
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
 
-  {/* //here we used spread operator(...) to import all the data from the mentioned data content  */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* //here we used spread operator(...) to import all the data from the mentioned data content  */}
+            {/* <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />  */}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            
             {/* //onSelect is passing as props to the TabButtons component */}
             {/* //for passing the appropriate value to the onClick event,
              here we r using it as an arrow function & passing an argument to it , 
              so that with that argument we can get to know which button was clicked */}
 
-             {/* here actually handleSelect is a function passed to the props onSelect,
+            {/* here actually handleSelect is a function passed to the props onSelect,
              but since we need to pass a argument to determine which handleSelect
              button was clicked, we converted the handleSelect function as arrow function 
-             & passed an argument to it for each of the function */}            
+             & passed an argument to it for each of the function */}
 
-            <TabButtons onSelect={() => handleSelect("components")}>
+            {/* here I am passing value to the isSelected props based on the condition,
+             that if the selectedTopic is components it is passed such like that */}
+
+            <TabButtons
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
               Components
             </TabButtons>
-            <TabButtons onSelect={() => handleSelect("jsx")}>JSX</TabButtons>
-            <TabButtons onSelect={() => handleSelect("props")}>
+            <TabButtons
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButtons>
+            <TabButtons
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
               Props
             </TabButtons>
-            <TabButtons onSelect={() => handleSelect("state")}>
+            <TabButtons
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
               State
             </TabButtons>
           </menu>
@@ -113,3 +132,17 @@ function App() {
 }
 
 export default App;
+
+// in this section we built this beautiful interactive demo web appear with React,
+//  while building this we learned about all the core concepts mentioned in the site...
+// we learned that react is all about components & we learned how to build react components,
+// components in the end are simply functions,
+// how to configure components with props
+// outputting dynamic content with curly braces
+// special children props, which allow us to share content between opening & closing tags of component
+// components , props & jsx are all super important features
+// listen to events
+// to update ui u must use state hook
+// it allows yo to manage & update ui
+// ternary operators- ? : , logical and &&
+// mapping function
--- 01-react-essentials/src/components/TabButtons.jsx
+++ 01-react-essentials/src/components/TabButtons.jsx
@@ -3,7 +3,8 @@ export default function (props) {
     <li>
       {/* //we r getting onSelect as props from the app.js,
        where here it is assigned for the onClick event handler */}
-      <button onClick={props.onSelect}>{props.children}</button>
+       {/* we r passing classname dynamically & using a prop isSelected here */}
+      <button className={props.isSelected ? 'active' : ''} onClick={props.onSelect}>{props.children}</button>
     </li>
   );
 }
