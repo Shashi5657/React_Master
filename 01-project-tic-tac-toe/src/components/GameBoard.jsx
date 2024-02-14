4354e1572e29a434c078d22edfebdbd49900e9ad
Shashi5657 <sangepushashidhar@gmail.com>
2024-02-14 20:16:48 +0530

    react tic-tac-toe project

--- 01-project-tic-tac-toe/src/components/GameBoard.jsx
+++ 01-project-tic-tac-toe/src/components/GameBoard.jsx
@@ -9,31 +9,31 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-  //creating this state to update the game board with (X, O) i.e., in  rows & columns
-  // initially we'll set the value to null, so using initialgameboard
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // //creating this state to update the game board with (X, O) i.e., in  rows & columns
+  // // initially we'll set the value to null, so using initialgameboard
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  //this function is used foe clicking simply for selecting square & putting our mark,
-  //so to know which square it was clicked, it takes tha arguments rowIndex and colIndex,
-  //rowIndex gives which array it is initially selecte,
-  //colIndex gives which item is selected from the array that is selected from rowIndex,
-  function handleSelectSquare(rowIndex, colIndex) {
-    // for updating the board we set this as setGameBoard, 
-    // initially it takes the prevGameBoard as an arguments
-    setGameBoard((prevGameBoard) => {
-        //when we r using arrays / objects we need to use spread operator(...),
-        //if we don't use then ui will directly get updated
-        //this spread operator makes a copy of the initialGameBoard,
-        //& we stored in the const updatedBoard, so we'll update it through this const
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+  // //this function is used foe clicking simply for selecting square & putting our mark,
+  // //so to know which square it was clicked, it takes tha arguments rowIndex and colIndex,
+  // //rowIndex gives which array it is initially selecte,
+  // //colIndex gives which item is selected from the array that is selected from rowIndex,
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   // for updating the board we set this as setGameBoard, 
+  //   // initially it takes the prevGameBoard as an arguments
+  //   setGameBoard((prevGameBoard) => {
+  //       //when we r using arrays / objects we need to use spread operator(...),
+  //       //if we don't use then ui will directly get updated
+  //       //this spread operator makes a copy of the initialGameBoard,
+  //       //& we stored in the const updatedBoard, so we'll update it through this const
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
 
-    onSelectSquare();
-  }
+  //   onSelectSquare();
+  // }
   return (
     <ol id="game-board">
       {/* initially we mapped initialGameBoard which contains three arrays , so it returns each 
@@ -49,7 +49,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
             like this we'll extract all items from 3 arrays */}
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={onSelectSquare}>
                   {playerSymbol}
                 </button>
               </li>
