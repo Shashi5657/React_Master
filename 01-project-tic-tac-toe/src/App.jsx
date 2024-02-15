abd7cf947413d0c7f8834ae2157e2bcdc9d70c7b
Shashi5657 <sangepushashidhar@gmail.com>
2024-02-15 13:48:23 +0530

    react tic-tac-toe project

--- 01-project-tic-tac-toe/src/App.jsx
+++ 01-project-tic-tac-toe/src/App.jsx
@@ -4,9 +4,21 @@ import Player from "./components/Player";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns
+    });
   }
   return (
     <main>
@@ -15,12 +27,12 @@ function App() {
           <Player
             initialName="Player 1"
             symbol="X"
-            isActive={activePlayer === 'X'}
+            isActive={activePlayer === "X"}
           />
           <Player
             initialName="Player 2"
             symbol="O"
-            isActive={activePlayer === 'O'}
+            isActive={activePlayer === "O"}
           />
         </ol>
         <GameBoard
