809781e9ab7aafa0ded1f01cf99174c697bc5009
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-03 21:03:07 +0530

    refs and portals

--- 05-refs-and-portals/src/components/ResultModal.jsx
+++ 05-refs-and-portals/src/components/ResultModal.jsx
@@ -4,6 +4,7 @@ const ResultModal = forwardRef(function ResultModal(
   { result, targetTime },
   ref
 ) {
+  
   return (
     <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
