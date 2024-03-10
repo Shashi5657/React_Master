04f56c6999017ab7afeb5f537fab610298416d19
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-10 11:29:17 +0530

    commit message

--- 05-refs-and-portals/src/components/ResultModal.jsx
+++ 05-refs-and-portals/src/components/ResultModal.jsx
@@ -9,6 +9,7 @@ const ResultModal = forwardRef(function ResultModal(
   const userLost = remainingTime <= 0;
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  
   const score = Math.round((1- remainingTime/(targetTime*1000))*100)
 
   useImperativeHandle(ref, () => {
