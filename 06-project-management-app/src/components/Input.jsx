c0bcc87634033d75c44d943c74dec228d532df65
Shashi5657 <sangepushashidhar@gmail.com>
2024-03-15 12:15:00 +0530

    new project started

--- 06-project-management-app/src/components/Input.jsx
+++ 06-project-management-app/src/components/Input.jsx
@@ -1,4 +1,5 @@
 export default function Input({ label, textarea, ...props }) {
+    
     const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 focus:outline-none focus:border-stone-600"
   return (
     <p className=" flex flex-col gap-1 my-4">
