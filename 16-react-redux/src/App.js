321c84a0c9065367e3248ffb2b2268df61360706
Shashi5657 <sangepushashidhar@gmail.com>
2024-04-25 08:14:28 +0530

    commit message

--- 16-react-redux/src/App.js
+++ 16-react-redux/src/App.js
@@ -1,3 +1,8 @@
+//using redux in this section
+//redux is a state managementr system for cross-component or app-wide state
+//which means we can manage state in any of the component
+//through a centralized store 
+
 import Counter from './components/Counter';
 
 
