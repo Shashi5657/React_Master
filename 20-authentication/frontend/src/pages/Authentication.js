ac5fe46607364cb32ae1becd912724f249864a9c
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-11 22:48:21 +0530

    will finish

--- 20-authentication/frontend/src/pages/Authentication.js
+++ 20-authentication/frontend/src/pages/Authentication.js
@@ -11,7 +11,7 @@ export async function action({ request }) {
   const searchParams = new URL(request.url).searchParams;
   const mode = searchParams.get('mode') || 'login' ;
 
-  if(mode !== 'login' || mode !== 'signup'){
+  if(mode !== 'login' && mode !== 'signup'){
     throw json({message : 'Unsupported mode'} , {status : 422})
   }
 
@@ -22,10 +22,10 @@ export async function action({ request }) {
     password: data.get("password"),
   };
 
-  const response = await fetch('http://localhost:8000/' + mode, {
+  const response = await fetch('http://localhost:8080/' + mode, {
     method : 'POST',
     headers : {
-      'Content_type' : 'application/json'
+      'Content-Type' : 'application/json'
     },
     body : JSON.stringify(authData)
   })
