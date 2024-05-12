44f9ae8d54e92ebad9231c40f1e0a9f970bb5352
Shashi5657 <sangepushashidhar@gmail.com>
2024-05-12 20:45:50 +0530

    authentication finished

--- 20-authentication/frontend/src/pages/Root.js
+++ 20-authentication/frontend/src/pages/Root.js
@@ -1,9 +1,34 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from "react";
+import {
+  Outlet,
+  useLoaderData,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if(token === 'EXPIRED'){
+      submit(null, { action: "/logout", method: "POST" });
+      return ;
+    }
+
+    const tokenDuration = getTokenDuration()
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "POST" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
